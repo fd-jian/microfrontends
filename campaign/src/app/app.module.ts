ab4fec78072d629f3af56515760171dc8a3ebd9c
unknown <markuswondrak@gmail.com>
2019-10-12 13:42:50 +0200

    turned ng app to web component

--- campaign/src/app/app.module.ts
+++ campaign/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  entryComponents: [AppComponent],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const customElement = createCustomElement(AppComponent, {injector: this.injector});
+    customElements.define('campaign-app', customElement);
+  }
+}
